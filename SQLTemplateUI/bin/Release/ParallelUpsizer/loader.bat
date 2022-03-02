8c957904ae85b15d6591a4fb29f5046b49f31e6b
ItsJobo <josephmurrells@gmail.com>
2022-03-02 23:04:01 +0000

    Initial Commit

--- SQLTemplateUI/bin/Release/ParallelUpsizer/loader.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\Job0\source\repos\SQLTemplateUI\SQLTemplateUI\bin\Release\ParallelUpsizer\ParallelUpsizer.exe C:\Users\Job0\source\repos\SQLTemplateUI\SQLTemplateUI\bin\Release\ParallelUpsizer\upsizerInput.json
